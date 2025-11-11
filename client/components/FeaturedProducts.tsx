12a41406b59bdbda6508bd932fca4f5a6c1fe856
MrLexsmyth <shittuodunayo4@gmail.com>
2025-11-11 14:50:38 +0100

    fix

--- client/components/FeaturedProducts.tsx
+++ client/components/FeaturedProducts.tsx
@@ -35,7 +35,7 @@ export default function FeaturedProducts() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch( `${process.env.NEXT_PUBLIC_API_URL}/products`, {
+        const res = await fetch( `${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
           credentials: "include",
         });
         if (!res.ok) throw new Error("Failed to fetch products");
