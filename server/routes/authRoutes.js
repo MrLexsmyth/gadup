c15da3195ccf63f25a738a958bf6ebe552221a62
MrLexsmyth <shittuodunayo4@gmail.com>
2025-11-12 19:26:06 +0100

    upddate

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const getCookieOptions = () => ({
   httpOnly: true,
   secure: process.env.NODE_ENV === "production", 
- sameSite: "none",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 });
 
